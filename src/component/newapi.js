32957c965bb5d9d94c4f3f1cc6cdec354aaae196
yunhwan <yunhwan.choo00@gmail.com>
2023-10-27 01:30:19 +0900

    cashing

--- src/component/newapi.js
+++ src/component/newapi.js
@@ -1,6 +1,5 @@
-//https://port-0-yazo2023-3prof2lll0d1goc.sel4.cloudtype.app/getInfo/
 //이 주소가 실제 프로젝트에서 쓰일 주소
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
 import './css/newapi.css'
 
@@ -10,14 +9,24 @@ function NewApi() {
 
   const locateArray = [0,25,224,382,61,92,49,324,211,756,111]; //버튼 넣고 싶은 셀 번호 목록
 
+  const cash = useMemo(() => {
+    const cash = new Map();
+    return cash;
+  },[])// 캐시 메모리
+  
   useEffect(() => {
-    axios.get(`https://port-0-yazo2023-3prof2lll0d1goc.sel4.cloudtype.app/getInfo/` + num)
+    if(cash.has(num)){
+      setData(cash.get(num))
+    } else {
+      axios.get(`https://port-0-yazo2023-3prof2lll0d1goc.sel4.cloudtype.app/getInfo/` + num)
       .then(response => {
         setData(response.data);
+        cash.set(num,response.data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
+    }
   }, [num]); //백엔드에 요청 날려서 데이터 받아오는 함수
 
   const renderCellValue = (cellIndex, rowIndex) => {
