9e9530f0a6665a78566ae5e45cbbf586aa1dcae9
Hoon <1669jsh@naver.com>
2023-12-05 14:31:07 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/main';
 import About from './pages/about/about';
 import Grid_bars from './component/grid_bar';
 import backimg from './img/back2.jpg'
 import NewApi from './component/newapi';
+import Credit from './pages/credit/credit';
+import Program from './pages/program/Program';
 
 function App() {
   return (
     <div className='app'>
-      <BrowserRouter basename='/2023yajo'>
+      <HashRouter>
         <Grid_bars back_img={backimg} />
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/about' element={<About />} />
-          <Route path='/archive' element={<NewApi />} />
+          <Route path='/archive' element={<NewApi />}/>
+          <Route path='/program' element={<Program/>}/>
+          <Route path='/credit' element={<Credit/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
--- src/component/newapi.js
+++ src/component/newapi.js
@@ -7,34 +7,34 @@ function NewApi() {
   const [data, setData] = useState(null); // 가져온 데이터가 담긴 변수
   const [num, setNum] = useState(1); //가져올 사람 번호가 담긴 변수
 
-  const locateArray = [0,458,381,371,457,383,605,375,566,388,411
-                        ,424,338,467,420,529,336,374,384,508,344
-                        ,215,292,346,385,345,421,548,427,423,376
-                        ,261,342,368,527]; //버튼 넣고 싶은 셀 번호 목록
+  const locateArray = [0, 458, 381, 371, 457, 383, 605, 375, 566, 388, 411
+    , 424, 338, 467, 420, 529, 336, 374, 384, 508, 344
+    , 215, 292, 346, 385, 345, 421, 548, 427, 423, 376
+    , 261, 342, 368, 527]; //버튼 넣고 싶은 셀 번호 목록
 
   const cash = useMemo(() => {
     const cash = new Map();
     return cash;
-  },[])// 캐시 메모리
-  
+  }, [])// 캐시 메모리
+
   useEffect(() => {
-    if(cash.has(num)){
+    if (cash.has(num)) {
       setData(cash.get(num))
     } else {
       axios.get(`https://port-0-yazo2023-3prof2lll0d1goc.sel4.cloudtype.app/getInfo/` + num)
-      .then(response => {
-        setData(response.data);
-        cash.set(num,response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+        .then(response => {
+          setData(response.data);
+          cash.set(num, response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
     }
   }, [num]); //백엔드에 요청 날려서 데이터 받아오는 함수
 
   const renderCellValue = (cellIndex, rowIndex) => {
     const calculatedValue = cellIndex + 40 * rowIndex + 1;
-    if(calculatedValue === 261 || calculatedValue === 342|| calculatedValue === 368|| calculatedValue === 527) {return 25;}
+    if (calculatedValue === 261 || calculatedValue === 342 || calculatedValue === 368 || calculatedValue === 527) { return 25; }
     return locateArray.indexOf(calculatedValue);
   }; // 셀 번호 계산해서 리턴하는 함수
 
@@ -46,7 +46,7 @@ function NewApi() {
   const handleCellClick = (cellIndex, rowIndex) => {
     setNum(renderCellValue(cellIndex, rowIndex));
   }; // 셀 번호로 num 바꿔주는 함수
-  
+
   return (
     <div className='api_container'>
       <table className='ApiTable'>
@@ -55,7 +55,7 @@ function NewApi() {
             <tr key={rowIndex}>
               {Array.from({ length: 40 }).map((_, cellIndex) => (
                 <td key={cellIndex} className='TdCell'>
-                  {haslocate(cellIndex, rowIndex) ? <button style={{width: '20px', height: '20px', fontSize:10, backgroundColor:'skyblue', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'}} onClick={() => handleCellClick(cellIndex, rowIndex)}>{renderCellValue(cellIndex,rowIndex)}</button> : null}
+                  {haslocate(cellIndex, rowIndex) ? <button style={{ width: '20px', height: '20px', fontSize: 10, backgroundColor: 'skyblue', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }} onClick={() => handleCellClick(cellIndex, rowIndex)}>{renderCellValue(cellIndex, rowIndex)}</button> : null}
                 </td>
               ))}
             </tr>
@@ -64,15 +64,15 @@ function NewApi() {
       </table>
       {data ? (
         <>
-        <p>{data.name}</p>
-        <p>{data.title}</p>
-        <p>{data.ing}</p>
-        <p>{data.size}</p>
-        <pre style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace' }}>
-          {data.text}
-        </pre>
-        <pre>{data.photoList}</pre>
-        <pre>{data.movieList}</pre>
+          <p>{data.name}</p>
+          <p>{data.title}</p>
+          <p>{data.ing}</p>
+          <p>{data.size}</p>
+          <pre style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace' }}>
+            {data.text}
+          </pre>
+          <pre>{data.photoList}</pre>
+          <pre>{data.movieList}</pre>
         </>
       ) : (
         <p>Loading...</p>
